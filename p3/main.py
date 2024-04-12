5a931e94002f5291cb553ab4e8d69d771bf270f6
Pacatro <paco.algar.15@gmail.com>
2024-04-12 13:17:58 +0200

    dejando bonito el main

--- p3/main.py
+++ p3/main.py
@@ -16,23 +16,19 @@ def main():
     
     data_labels = [False, False, False, False, True, True, True, True]
     
-    # data = [[1, 1]]
-    # data_labels = [False]
-    
     print(f"Labels: {data_labels}\n")
     
     perceptron = Perceptron(data, data_labels)
-    # perceptron.set_weights([2, 3, -4])
     
-    initial_weights = perceptron.get_weights()
-    initial_pred = perceptron.predict()
-    initial_error = perceptron.error()
+    print(f"Initial weights: {perceptron.get_weights()}")
+    print(f"Initial prediction: {perceptron.predict()}")
+    print(f"Initial error: {perceptron.error()}\n")
     
     # Ajust the perceptron
     epochs = 1000
     learning_rate = 0.01
     
-    print(f"Ajusting the perceptron for {epochs} epochs with a learning rate of {learning_rate}\n")
+    print(f"Ajusting perceptron for {epochs} epochs with a learning rate of {learning_rate}\n")
     
     start = time.time()
     perceptron.ajust(epochs=epochs, learning_rate=learning_rate)
@@ -40,51 +36,34 @@ def main():
     
     ajust_time = end - start
     
-    ajusted_weights = perceptron.get_weights()
-    ajusted_pred = perceptron.predict()
-    ajusted_error = perceptron.error()
-        
-    print(f"Initial weights: {initial_weights}")
-    print(f"Initial prediction: {initial_pred}")
-    print(f"Initial error: {initial_error}\n")
+    print(f"Ajusted weights: {perceptron.get_weights()}")
+    print(f"Ajusted prediction: {perceptron.predict()}")
+    print(f"Ajusted error: {perceptron.error()}\n")
     
-    print(f"Ajusted weights: {ajusted_weights}")
-    print(f"Ajusted prediction: {ajusted_pred}")
-    print(f"Ajusted error: {ajusted_error}\n")
-    
-    print(f"Perceptron time: {ajust_time} seconds")
+    print(f"Perceptron time: {ajust_time} seconds\n")
     
     logistical_classification = LogisticalClassification(data, data_labels)
-    
-    initial_weights = logistical_classification.get_weights()
-    initial_pred = logistical_classification.predict(0.5)
-    initial_error = logistical_classification.error()
     
     # Ajust the logistical_classification
     epochs = 10000
     learning_rate = 0.01
     threshold = 0.5
     
-    print(f"\nAjusting the logistical_classification for {epochs} epochs with a learning rate of {learning_rate}\n")
+    print(f"Initial weights: {logistical_classification.get_weights()}")
+    print(f"Initial prediction: {logistical_classification.predict(0.5)}")
+    print(f"Initial error: {logistical_classification.error()}\n")
+    
+    print(f"Ajusting logistical classification for {epochs} epochs with a learning rate of {learning_rate} with a threshold of {threshold}\n")
+    
     start = time.time()
     logistical_classification.ajust(epochs=epochs, learning_rate=learning_rate)
     end = time.time()
     
     ajust_time = end - start
     
-    ajusted_weights = logistical_classification.get_weights()
-    ajusted_pred = logistical_classification.predict(threshold)
-    ajusted_error = logistical_classification.error()
-    
-    print(f"Threshold: {threshold}\n")
-    
-    print(f"Initial weights: {initial_weights}")
-    print(f"Initial prediction: {initial_pred}")
-    print(f"Initial error: {initial_error}\n")
-    
-    print(f"Ajusted weights: {ajusted_weights}")
-    print(f"Ajusted prediction: {ajusted_pred}")
-    print(f"Ajusted error: {ajusted_error}\n")
+    print(f"Ajusted weights: {logistical_classification.get_weights()}")
+    print(f"Ajusted prediction: {logistical_classification.predict(threshold)}")
+    print(f"Ajusted error: {logistical_classification.error()}\n")
     
     print(f"Logistical Classification time: {ajust_time} seconds")
     
